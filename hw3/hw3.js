07f9e21834557929e0b6effd7323310c0cd41463
Chongzhi Gao <c712g285@ku.edu>
2019-09-30 16:50:53 -0500

    init repos

---
+++ hw3/hw3.js
@@ -0,0 +1,18 @@
+var x;
+var j = 2;
+var m_arr;
+for (var i = 0; i <= x; i++) {
+  m_arr[i] = j;
+  j++
+}
+SiveOfEratosthenes = function ()
+{
+  if (true) {
+
+  }
+  else {
+
+  }
+
+
+}
---
+++ hw3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int m;
+  cin>>m;
+  int initParam = 2;
+  int m_arr[m];
+  for (int i = 0; initParam <= m; i++)
+  {
+    m_arr[i] = initParam;
+    initParam++;
+  }
+  for (int i = 0; i < m-1; i++)
+  {
+    cout<<m_arr[i]<<",";
+  }
+  cout<<endl;
+  if (true)
+  {
+  }
+  else
+  {
+  }
+return 0;
+}
+
+int SiveOfEratosthenes()
+{
+  if (true)
+  {
+  }
+  else
+  {
+  }
+  return 0;
+}
